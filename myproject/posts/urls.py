f7e1392fd34ecc7c18552459da194b3038465acb
Sha7ata-Mohamed <sha7ata1379@gmail.com>
2025-12-12 19:27:12 +0300

    d
    d

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
+    path("", views.posts_list, name="list"),
+    path("<slug:slug>", views.post_page, name="page"),
 ]
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
-
+from .models import Post
 # Create your views here.
-def post_list(request):
-    return render(request, "posts/posts_list.html")
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, "posts/posts_list.html", {"posts": posts})
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post': post})
+
+

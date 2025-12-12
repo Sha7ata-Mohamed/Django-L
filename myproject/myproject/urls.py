3af78e3144ce098111ccacbbeef01a1106430fac
Sha7ata-Mohamed <sha7ata1379@gmail.com>
2025-12-12 03:42:47 +0300

    d
    d

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.homepage, name="home"),
     path("about/", views.about, name="about"),
+    path("posts/", include("posts.urls")),
 ]
-
